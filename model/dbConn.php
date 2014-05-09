400ff6738dbcc5b0409eb601cf6d152c0fb2c52c
Nicolas Far <nicofff@gmail.com>
2014-05-09 10:46:57 -0300

    Finally fix conenction fallback

--- model/dbConn.php
+++ model/dbConn.php
@@ -7,7 +7,7 @@ $DB_PRIMARY_SERVER= "db";
 $DB_SECONDARY_SERVER= "web";
 
 $MYSQLI = new mysqli($DB_PRIMARY_SERVER,$DB_USER,$DB_PASS,$DB_NAME);
-if (!$MYSQLI->connect_errno){
+if ($MYSQLI->connect_errno){
     $MYSQLI = new mysqli($DB_SECONDARY_SERVER,$DB_USER,$DB_PASS,$DB_NAME);    
 }
 $MYSQLI->set_charset("utf8");
