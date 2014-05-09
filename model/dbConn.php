f8e2531320f6ae4f887698bf54f739d8ce776c97
Nicolas Far <nicofff@gmail.com>
2014-05-09 10:29:14 -0300

    Fix Typo

--- model/dbConn.php
+++ model/dbConn.php
@@ -7,7 +7,7 @@ $DB_PRIMARY_SERVER= "db";
 $DB_SECONDARY_SERVER= "web";
 
 $MYSQLI = new mysqli($DB_PRIMARY_SERVER,$DB_USER,$DB_PASS,$DB_NAME);
-if (!$DB){
+if (!$MYSQLI){
     $MYSQLI = new mysqli($DB_SECONDARY_SERVER,$DB_USER,$DB_PASS,$DB_NAME);    
 }
 $MYSQLI->set_charset("utf8");
