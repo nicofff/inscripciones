11589ccf1be89fb7779f568c67be83db0c4bfd75
Nicolas Far <nicofff@gmail.com>
2014-05-09 10:36:09 -0300

    update credentials, fix fallback

--- model/dbConn.php
+++ model/dbConn.php
@@ -1,13 +1,13 @@
 <?php
 
-$DB_USER= "root";
-$DB_PASS = "InfraYVirt";
+$DB_USER= "inscripciones";
+$DB_PASS = "inscripciones";
 $DB_NAME =  "inscripciones";
 $DB_PRIMARY_SERVER= "db";
 $DB_SECONDARY_SERVER= "web";
 
 $MYSQLI = new mysqli($DB_PRIMARY_SERVER,$DB_USER,$DB_PASS,$DB_NAME);
-if (!$MYSQLI){
+if (!$MYSQLI->connect_error){
     $MYSQLI = new mysqli($DB_SECONDARY_SERVER,$DB_USER,$DB_PASS,$DB_NAME);    
 }
 $MYSQLI->set_charset("utf8");
